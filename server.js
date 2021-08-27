baf89908c4b97e0a882897949fc2901a49b59794
Deepesh Padala <padaladeepesh@gmail.com>
2021-08-28 01:51:43 +0530

    completed, just gotta change some stuff and deploy

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require("express");
+//const express = require("express");
 const admin = require("firebase-admin");
 const spreadsheets = require("./spreadsheet.js");
 const serviceAccount = require("./serviceAccount.json");
@@ -7,18 +7,40 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
-const app = express();
+//const app = express();
 const db = admin.firestore();
 
+function json_from_spreadsheet(val) {
+    console.log(val);
+    return {
+        email: val[0],
+        index: val[1],
+        house: val[2]
+    };
+}
 
-
-app.get('/', async (req, res) => {
+async function check_and_add_users() {
+    const usersRef = db.collection("users");
     const vals = await spreadsheets.accessSpreadsheet();
     var count = 0;
-    vals.forEach(val => {
-        count += 1;
-    });
+    await vals.forEach(async val => {
+        const email = val[0];
+        let snapshot = await usersRef.where("email", '==', email).get();
+        if (snapshot.empty) {
+            console.log(`${email} not found... adding it to the database`);
+            let user_json = json_from_spreadsheet(val);
+            usersRef.add(user_json);
+        }
+    })
+    return count;
+} 
+
+setInterval(() => check_and_add_users(), 5*60*1000);
+/*
+app.get('/', async (req, res) => {
+    const count = await check_and_add_users();
     res.send({count});
 });
 
 app.listen(3000, () => console.log("listening on port 3000"));
+*/
